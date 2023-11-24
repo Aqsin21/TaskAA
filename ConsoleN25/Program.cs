2c790a5fc9f9c52f233f85f4d12ce118067378e6
Aqsin21 <agshinaa@code.edu.az>
2023-11-24 19:11:31 +0400

    uahd

---
+++ ConsoleN25/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace ConsoleN25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 3333;
+
+
+            Console.WriteLine(a * 10 + 700008);
+
+
+            //task2 
+            int b = 333;
+            Console.WriteLine(b * 1000 + b);
+            //task3 
+            int c = 10000;
+            Console.WriteLine(((c * 18) / 100));
+            Console.WriteLine((c * 3) / 100);
+
+            //task4
+            int d = 100;
+            Console.WriteLine(((d * 10 + 7) * 7) / 100);
+
+
+            //task5
+            int e = 1000;
+            Console.WriteLine(e * 100 + 4000044);
+            //task6
+            int f = 1000;
+            Console.WriteLine((((f * 20) / 100) * 10) / 100);
+
+
+            //task7
+            int g = 10000;
+            int g1 = 20000;
+            Console.WriteLine((g + g1) * 10 + 5000005);
+
+
+            //task8
+            int h = 1000;
+            int h1 = 1000000;
+          int h2 = (h  *4) / 100;
+            int h3 = (h1 * 9) / 100;
+            Console.WriteLine(((h2 + h3) * 10) / 100);
+
+
+            //task9 
+            int i = 100000;
+            int i1 = 200000;
+            int i2 = 300000;
+            int i3 = (i * 10) / 100;
+            int i4 = (i1 * 10) / 100;
+            int i5=(i2 * 10) / 100;
+            Console.WriteLine(((i3 + i4 + i5) * 10) / 100);
+
+
+            //task10
+            int j = 1000;
+            int j1 = 2000;
+            int j2 = 3000;
+            int j3 = (j * 1) / 100;
+            int j4 = (j1 * 2) / 100;
+            int j5 = (j2 * 3) / 100;
+            int j6 = (j2 * 7) / 100;
+            Console.WriteLine((j5 - j4 - j3) + j6);
+            Console.ReadKey();
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+        
+        
+    
+
